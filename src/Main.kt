859aa8e1dc49b20e13b9c677644449d1fc5ccfcc
Verdinho-IMA <leonardo22rami@gmail.com>
2024-02-18 14:44:49 -0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+package fundamentos
+fun main(args: Array<String>) {
+
+    var a: Int = 63.dec()
+    var b: String = a.toString()
+
+    println("Int: $a")
+
+    println("Primeiro char da String b é: ${b.first()}")
+}
